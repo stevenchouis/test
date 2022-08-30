4961a4b55d3970286cdb41e3809a5fcd01f2d0f2
stevenchouis <stevenchouis@hotmail.com>
2022-08-30 20:25:25 +0800

    second

--- package_test1.py
+++ package_test1.py
@@ -1,6 +1,8 @@
 # 直接import package, 它會去import該目錄下的__init__.py
 # 再使用該Package的__init__.py中所define的class
+from telnetlib import STATUS
 import package1 as pkg1
 
 human1 = pkg1.Human("Steven", "male", "Taiwanese")
 human1.speak("Taiwanese")
+human1.speak("English")
