ce3bb3d69fa8f7908b2d4ad961cb9636d00b2e72
stevenchouis <stevenchouis@hotmail.com>
2022-08-30 20:47:29 +0800

    update tools

--- tools.py
+++ tools.py
@@ -6,3 +6,7 @@ def calculate_triangle_third_edge(edge_1, edge_2, is_third_edge_long_edge=True):
         return math.sqrt(edge_1*edge_1 + edge_2*edge_2)
     else:
         return math.sqrt(abs(edge_1*edge_1 - edge_2*edge_2))
+
+
+def test():
+    print("test")
